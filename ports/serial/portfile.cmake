6c1e7cb80d8fb8d2d8fda50c59c8d28226175ae2
myd7349 <myd7349@gmail.com>
2018-10-18 19:46:02 +0800

    [serial] Add new port

---
+++ ports/serial/portfile.cmake
@@ -0,0 +1,10 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO wjwwood/serial
+    REF 5a354eaab7c064a155857d377a2fa3f0f8390c34
+    SHA512 e593d206b594aacca97656ca6d5a31e7f7679b75a943964e8348c882d529e2df0187b7e5091e83465e50eb146279d557024fa6b31e7d9aae84361e527b5c7cbe
+    HEAD_REF master
+)
+
